04dc30e85c3c5c49cea0084d2371c8a2aa92fda9
Martin v Z <github@cubeactive.com>
2014-04-18 14:38:41 +0200

    Resource could not be found without the package name

--- library/src/com/cubeactive/affiliateslibrary/AffiliatesAppsProvider.java
+++ library/src/com/cubeactive/affiliateslibrary/AffiliatesAppsProvider.java
@@ -10,6 +10,7 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.util.Log;
@@ -21,7 +22,7 @@ public class AffiliatesAppsProvider {
 	public AffiliatesAppsProvider() {
 	}
 	
-	private static AffiliatesApp parseAppTag(final XmlResourceParser aXml, final Resources aResources) throws XmlPullParserException, IOException {
+	private static AffiliatesApp parseAppTag(final XmlResourceParser aXml, final Resources aResources, final String aPackagename) throws XmlPullParserException, IOException {
 		final AffiliatesApp _Result = new AffiliatesApp();
 		
 		//Parse attributes
@@ -39,17 +40,17 @@ public class AffiliatesAppsProvider {
 		final String _FormattedPackageName = _Result.getPackageName().replace(".", "_");
 		//Get title
 		final String _TitleResourceName = "affiliate_app_title_" + _FormattedPackageName;
-		int _ResId = aResources.getIdentifier( _TitleResourceName , "string", null);
+		int _ResId = aResources.getIdentifier( _TitleResourceName , "string", aPackagename);
 		_Result.setDescription(aResources.getString(_ResId));
 		
 		//Get description
 		final String _DescriptionResourceName = "affiliate_app_description_" + _FormattedPackageName;
-		_ResId = aResources.getIdentifier( _DescriptionResourceName , "string", null);
+		_ResId = aResources.getIdentifier( _DescriptionResourceName , "string", aPackagename);
 		_Result.setDescription(aResources.getString(_ResId));
 		
 		//Find app icon resource id
 		final String _IconResourceName = "ic_app_" + _FormattedPackageName;
-		_ResId = aResources.getIdentifier( _IconResourceName , "drawable", null);
+		_ResId = aResources.getIdentifier( _IconResourceName , "drawable", aPackagename);
 		_Result.setIcon(_ResId);
 		
 		//Move to the next tag
@@ -61,16 +62,25 @@ public class AffiliatesAppsProvider {
 	}
 	
 	public static List<AffiliatesApp> getAffiliatesApps(final Context context) {
+    	//Get resources
+    	final String _PackageName = context.getPackageName();
+    	Resources _Res;
+		try {
+			_Res = context.getPackageManager().getResourcesForApplication(_PackageName);
+		} catch (final NameNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		} 
+		
 		final List<AffiliatesApp> _Result = new ArrayList<AffiliatesApp>();
-		final Resources _Res = context.getResources();
         //Get affiliates xml resource id
-      	final int _resID = _Res.getIdentifier(AFFILIATES_XML, "xml", null);		
+      	final int _resID = _Res.getIdentifier(AFFILIATES_XML, "xml", _PackageName);		
     	final XmlResourceParser _xml = _Res.getXml(_resID);
     	try {
             int eventType = _xml.getEventType();
             while (eventType != XmlPullParser.END_DOCUMENT) {
             	if ((eventType == XmlPullParser.START_TAG) && (_xml.getName().equals("app"))){
-            		_Result.add(parseAppTag(_xml, _Res));      		
+            		_Result.add(parseAppTag(_xml, _Res, _PackageName));      		
 	            }
             	eventType = _xml.next();
             }
