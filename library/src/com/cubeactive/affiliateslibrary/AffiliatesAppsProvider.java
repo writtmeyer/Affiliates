2cca7cdda77eaf8e5e8f0306e9ab1b7d37874767
Martin v Z <github@cubeactive.com>
2014-04-19 13:14:02 +0200

    Added async function to get a affliates apps list

--- library/src/com/cubeactive/affiliateslibrary/AffiliatesAppsProvider.java
+++ library/src/com/cubeactive/affiliateslibrary/AffiliatesAppsProvider.java
@@ -13,6 +13,7 @@ import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
+import android.os.AsyncTask;
 import android.util.Log;
 
 public class AffiliatesAppsProvider {
@@ -21,7 +22,7 @@ public class AffiliatesAppsProvider {
     
 	public AffiliatesAppsProvider() {
 	}
-	
+		
 	private static AffiliatesApp parseAppTag(final XmlResourceParser aXml, final Resources aResources, final String aPackagename) throws XmlPullParserException, IOException {
 		final AffiliatesApp _Result = new AffiliatesApp();
 		
@@ -61,6 +62,9 @@ public class AffiliatesAppsProvider {
         return _Result;
 	}
 	
+	//Function for loading a affiliates apps list.
+	//Use GetAffiliatesAppsAsyncTask() to load the list in a background thread, using this call
+	//might block the UI thread.
 	public static List<AffiliatesApp> getAffiliatesApps(final Context context) {
     	//Get resources
     	final String _PackageName = context.getPackageName();
@@ -71,16 +75,20 @@ public class AffiliatesAppsProvider {
 			e.printStackTrace();
 			return null;
 		} 
-		
+
+		return getAffiliatesApps(_Res, _PackageName);		
+	}
+
+	protected static List<AffiliatesApp> getAffiliatesApps(final Resources aResources, final String aPackagename) {
 		final List<AffiliatesApp> _Result = new ArrayList<AffiliatesApp>();
         //Get affiliates xml resource id
-      	final int _resID = _Res.getIdentifier(AFFILIATES_XML, "xml", _PackageName);		
-    	final XmlResourceParser _xml = _Res.getXml(_resID);
+      	final int _resID = aResources.getIdentifier(AFFILIATES_XML, "xml", aPackagename);		
+    	final XmlResourceParser _xml = aResources.getXml(_resID);
     	try {
             int eventType = _xml.getEventType();
             while (eventType != XmlPullParser.END_DOCUMENT) {
             	if ((eventType == XmlPullParser.START_TAG) && (_xml.getName().equals("app"))){
-            		_Result.add(parseAppTag(_xml, _Res, _PackageName));      		
+            		_Result.add(parseAppTag(_xml, aResources, aPackagename));      		
 	            }
             	eventType = _xml.next();
             }
@@ -97,9 +105,57 @@ public class AffiliatesAppsProvider {
     	Collections.shuffle(_Result, new Random());
 		return _Result;		
 	}
-
-	public static List<AffiliatesApp> getASyncAffiliatesApps(final Context context) {
-		//TODO: Add async function for loading a affiliates apps list.
-		return null;
+	
+	//Async function for loading a affiliates apps list.
+	public static GetAffiliatesAppsAsyncTask getASyncAffiliatesApps(final Context context, final Callbacks aCallback) {
+		final GetAffiliatesAppsAsyncTask task = new GetAffiliatesAppsAsyncTask(context, aCallback);
+        task.execute("");
+        return task;
 	}
+	
+    public static class GetAffiliatesAppsAsyncTask extends AsyncTask<String, Void, List<AffiliatesApp>> {
+    	private final String TAG = "GetAffiliatesAppsAsyncTask";
+    	private Resources mResources;
+    	private final String mPackagename;
+    	private final Callbacks mCallback;
+    	
+        public GetAffiliatesAppsAsyncTask(final Context context, final Callbacks aCallback) {
+        	mCallback = aCallback;
+        	//Get resources
+        	mPackagename = context.getPackageName();
+    		try {
+    			mResources = context.getPackageManager().getResourcesForApplication(mPackagename);
+    		} catch (final NameNotFoundException e) {
+    			e.printStackTrace();
+    		} 
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected List<AffiliatesApp> doInBackground(final String... params) {
+        	if (mResources == null)
+        		return null;
+            return getAffiliatesApps(mResources, mPackagename);
+        }
+
+        @Override
+        protected void onPostExecute(final List<AffiliatesApp> result) {
+        	if ((!isCancelled()) && (mCallback != null))
+        			mCallback.onAffiliatesAppsLoadFinished(result);
+        }
+
+    }
+	
+    /**
+	 * A callback interface for the async method getASyncAffiliatesApps
+	 */
+	public interface Callbacks {
+		/**
+		 * Callback for when an note has been selected.
+		 */
+		public void onAffiliatesAppsLoadFinished(List<AffiliatesApp> aAffiliatesApps);
+	}        
 }
